a48bea3403d8b801d1013f03aca5b532adaedcc3
Abo_Tolba <122931774+AhmedElbassuony@users.noreply.github.com>
2024-10-29 23:48:38 +0300

    Test

---
+++ CMD/src/test/java/CommandTest.java
@@ -0,0 +1,10 @@
+import org.example.Command;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions.*;
+
+public class CommandTest {
+  @Test
+  public void test() {
+    
+  }
+}
