059bff5af98cf41fcad65a40e5280bd5b084567b
mostafa <motttarek101@gmail.com>
2024-10-30 23:51:52 +0300

    Update CommandTest.java

--- CMD/src/test/java/CommandTest.java
+++ CMD/src/test/java/CommandTest.java
@@ -4,7 +4,66 @@ import org.junit.jupiter.api.Assertions.*;
 
 public class CommandTest {
   @Test
-  public void test() {
-    
-  }
+ public void testRemoveDirectory() {
+        
+        Path testDir = Paths.get(System.getProperty("user.dir")).resolve("testDir");
+
+      
+        try {
+            Files.deleteIfExists(testDir);
+            Files.createDirectory(testDir); 
+        } catch (IOException e) {
+            fail("Failed to set up for the test");
+        } 
+
+       
+        Main.RemoveDirectory(new ArrayList<>(List.of("testDir")), Paths.get(System.getProperty("user.dir")));
+
+       
+        assertTrue(Files.exists(testDir), "The directory should still exist because it is not a file");
+    }
 }
+  @Test
+ public void testMakeDirectory() {
+        
+        Path testDir = Paths.get(System.getProperty("user.dir")).resolve("testDir");
+
+        
+        try {
+            Files.deleteIfExists(testDir); 
+        } catch (IOException e) {
+            fail("Failed to clean up before the test");
+        }
+
+        
+        Main.makeDirectory(new ArrayList<>(List.of("testDir")), Paths.get(System.getProperty("user.dir")));
+
+        
+        assertTrue(Files.exists(testDir), "The directory should be created");
+
+        
+}
+ public void testRemoveFile() {
+        
+        Path testFile = Paths.get(System.getProperty("user.dir")).resolve("testFile.txt");
+
+        
+        try {
+            Files.deleteIfExists(testFile);
+        } catch (IOException e) {
+            fail("Failed to clean  before doing the test");
+        }
+
+        
+        try {
+            Files.createFile(testFile);
+        } catch (IOException e) {
+            fail("Failed to create the file");
+        }
+
+        
+        Main.removeFile(new ArrayList<>(List.of("testFile.txt")), Paths.get(System.getProperty("user.dir")));
+
+      
+        assertFalse(Files.exists(testFile), "The file should be removed");
+    }
